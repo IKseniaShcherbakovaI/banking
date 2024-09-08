63e765e12762832b9b777ba1296ef83512de444c
ксения щербакова <ksenia75423@mail.ru>
2024-09-08 16:20:58 +1000

    add services.py fix

--- src/services.py
+++ src/services.py
@@ -18,6 +18,8 @@ file_handler = logging.FileHandler(logs_file_path, "w", encoding="utf-8")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
+
+
 def transaction_excel(path_excel):
     """
     Функция принимающая на вход путь до excel файла и возвращающая список словарей с данными о финансовых транзакциях
@@ -44,16 +46,9 @@ def transactions_by_phone_number(transaction):
     result_search = []
     logger.info("получаем информацию по ключу")
     for i in transaction:
-        # logger.info("получаем информацию по ключу")
         our_str = i['Описание']
         if re.findall('.+7 9', our_str, re.IGNORECASE):
             result_search.append(i)
     logger.info("поиск строки выполнен, обрабатываем результат")
-    # data = json.dumps(result_search, indent=3, ensure_ascii=False)
     data = json.dumps(result_search, ensure_ascii=False)
-    # print(len(result_search))
     return data
-    # return result_search
-
-
-print(transactions_by_phone_number(result))
