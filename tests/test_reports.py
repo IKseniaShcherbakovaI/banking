e8223580150f8bbd5a1d74706c1870d715ea191f
ксения щербакова <ksenia75423@mail.ru>
2024-09-12 01:38:10 +1000

    add test_reports.py

---
+++ tests/test_reports.py
@@ -0,0 +1,41 @@
+import pytest
+import pandas as pd
+from src.reports import spending_by_category
+
+
+@pytest.fixture
+def sample_data():
+    data = {
+        "Дата операции": [
+            "01.12.2021 12:00:00",
+            "15.12.2021 10:30:00",
+            "25.12.2021 18:45:00",
+            "05.01.2022 08:00:00",
+            "20.02.2022 16:20:00",
+        ],
+        "Категория": ["Продукты", "Продукты", "Транспорт", "Продукты", "Транспорт"],
+        "Сумма": [100, 200, 50, 150, 80],
+    }
+    df = pd.DataFrame(data)
+    return df
+
+
+def test_spending_by_category_with_date(sample_data):
+    # Тестирование функции с указанной датой и категорией "Продукты"
+    result = spending_by_category(sample_data, "Продукты", "30.12.2021")
+    assert (
+        len(result) == 2
+    )
+
+
+def test_spending_by_category_no_date(sample_data):
+    result = spending_by_category(sample_data, "Продукты")
+    assert len(result) == 0
+
+
+
+
+def test_spending_by_category_no_transactions(sample_data):
+    # Тестирование функции с категорией, для которой нет транзакций
+    result = spending_by_category(sample_data, "Здоровье", "30.12.2021")
+    assert len(result) == 0
